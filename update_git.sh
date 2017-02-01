5719a2ee2a53175600a27ab0c7f24af20dd09822
Ludwig Nussel <ludwig.nussel@suse.de>
2017-02-01 10:43:28 +0100

    Adjusted for rpmlint

--- update_git.sh
+++ update_git.sh
@@ -1,25 +1,36 @@
-#!/bin/bash
+#!/bin/bash -e
 #
-# Instead of a quilt workflow, we use a git tree that contains
-# all the commits on top of a stable tarball.
+# based on qemu's update_git.sh this program updates the patches
+# applied on top of a tarball based on commmits in git
 #
-# When updating this package, just either update the git tree
-# below (use rebase!) or change the tree path and use your own
-#
-# That way we can easily rebase against the next stable release
-# when it comes.
+# how to use:
+# quilt setup rpmlint.spec
+# cp rpmlint-$RPMLINTVERSION/series .
+# mkdir ~/git; cd ~/git
+# git clone git://git.code.sf.net/p/rpmlint/code rpmlint-code
+# git checkout -b opensuse-$RPMLINTVERSION v$RPMLINTVERSION
+# git quiltimport --patches /where/rpmlint/checkout/is
+# ... add/remove/rebase patches
+# ... to rebase to a new version create branch and modify versions below
+# when done run update_git.sh
 
-set -e
+GIT_TREE=https://github.com/lnussel/rpmlint-code.git
+GIT_LOCAL_TREE=~/git/rpmlint-code
+GIT_BRANCH=opensuse-1.8
+GIT_UPSTREAM_TAG=rpmlint-1.8
 
-GIT_TREE=git://github.com/openSUSE/qemu.git
-GIT_LOCAL_TREE=~/git/qemu-opensuse
-GIT_BRANCH=opensuse-2.8
-GIT_UPSTREAM_TAG=v2.8.0
-GIT_DIR=/dev/shm/qemu-factory-git-dir
-CMP_DIR=/dev/shm/qemu-factory-cmp-dir
+cleanup()
+{
+    [ -z "$GIT_DIR" ] || rm -rf "$GIT_DIR"
+    [ -z "$CMP_DIR" ] || rm -rf "$GIT_DIR"
+}
 
-rm -rf $GIT_DIR
-rm -rf $CMP_DIR
+trap cleanup EXIT
+
+GIT_DIR=`mktemp -d --tmpdir update_git.XXXXXXXXXX`
+CMP_DIR=`mktemp -d --tmpdir update_git.XXXXXXXXXX`
+
+rm -f .update_git.*
 
 if [ -d "$GIT_LOCAL_TREE" ]; then
     echo "Processing $GIT_BRANCH branch of local git tree, using tag:" \
@@ -32,8 +43,8 @@ if [ -d "$GIT_LOCAL_TREE" ]; then
     git clone -ls $GIT_LOCAL_TREE $GIT_DIR -b $GIT_BRANCH
     if ! (cd $GIT_LOCAL_TREE && git remote show upstream &>/dev/null); then
         echo "Remote for upstream git tree not found. Next time add remote" \
-             "named upstream for git://git.qemu.org/qemu.git and update"
-        (cd $GIT_DIR && git remote add upstream git://git.qemu-project.org/qemu.git)
+             "named upstream for $GIT_TREE and update"
+        (cd $GIT_DIR && git remote add upstream "$GIT_TREE")
         (cd $GIT_DIR && git remote update)
    fi
 else
@@ -42,117 +53,99 @@ else
     echo "(For much fast processing, consider establishing a local git tree" \
          "at $GIT_LOCAL_TREE)"
     git clone $GIT_TREE $GIT_DIR -b $GIT_BRANCH
-    (cd $GIT_DIR && git remote add upstream git://git.qemu-project.org/qemu.git)
+    (cd $GIT_DIR && git remote add upstream "$GIT_TREE")
     (cd $GIT_DIR && git remote update)
 fi
-(cd $GIT_DIR && git format-patch -N $GIT_UPSTREAM_TAG --suffix= -o $CMP_DIR >/dev/null)
-QEMU_VERSION=`cat $GIT_DIR/VERSION`
-echo "QEMU version: $QEMU_VERSION"
+(cd $GIT_DIR && git format-patch -N $GIT_UPSTREAM_TAG --suffix=.tmp -o $CMP_DIR >/dev/null)
 
-rm -rf $GIT_DIR
+CHANGED_COUNT=0
+UNCHANGED_COUNT=0
+DELETED_COUNT=0
+ADDED_COUNT=0
 
-(
-    CHANGED_COUNT=0
-    UNCHANGED_COUNT=0
-    DELETED_COUNT=0
-    ADDED_COUNT=0
+shopt -s nullglob
 
-    shopt -s nullglob
-
-# Process patches to eliminate useless differences: limit file names to 40 chars
-# before extension and remove git signature. ('30' below gets us past dir prefix)
-    for i in $CMP_DIR/*; do
-        # format-patch may append a signature, which per default contains the git version
-        # wipe everything starting from the signature tag
-        sed '/^-- $/Q' $i > $CMP_DIR/${i:30:40}.patch
-        rm $i
-    done
-
-    for i in 0???-*.patch; do
-        if [ -e $CMP_DIR/$i ]; then
-            if cmp -s $CMP_DIR/$i $i; then
-                rm $CMP_DIR/$i
-                let UNCHANGED_COUNT+=1
-            else
-                mv $CMP_DIR/$i .
-                let CHANGED_COUNT+=1
-            fi
-        else
-            osc rm --force $i
-            let DELETED_COUNT+=1
-            echo "  ${i##*/}" >> qemu.changes.deleted
-        fi
-    done
-
-    for i in $CMP_DIR/*; do
-        mv $i .
-        osc add ${i##*/}
-        let ADDED_COUNT+=1
-        echo "  ${i##*/}" >> qemu.changes.added
-    done
-
-    for package in qemu qemu-linux-user; do
-        while IFS= read -r line; do
-            if [ "$line" = "PATCH_FILES" ]; then
-                for i in 0???-*.patch; do
-                    NUM=${i%%-*}
-                    echo -e "Patch$NUM:      $i"
-                done
-            elif [ "$line" = "PATCH_EXEC" ]; then
-                for i in 0???-*.patch; do
-                    NUM=${i%%-*}
-                    echo "%patch$NUM -p1"
-                done
-            elif [ "$line" = "QEMU_VERSION" ]; then
-                echo "Version:        $QEMU_VERSION"
-            elif [[ "$line" =~ ^Source: ]]; then
-                QEMU_TARBALL=qemu-`echo "$line" | cut -d '-' -f 2-`
-                VERSION_FILE=${QEMU_TARBALL%.tar.bz2}/roms/seabios/.version
-                SEABIOS_VERSION=`tar jxfO "$QEMU_TARBALL" "$VERSION_FILE"`
-                SEABIOS_VERSION=`echo $SEABIOS_VERSION | cut -d '-' -f 2`
-                echo "$line"
-            elif [ "$line" = "SEABIOS_VERSION" ]; then
-                echo "Version:        $SEABIOS_VERSION"
-            else
-                echo "$line"
-            fi
-        done < $package.spec.in > $package.spec
-
-        # Factory requires all deleted and added patches to be mentioned
-        if [ -e qemu.changes.deleted ] || [ -e qemu.changes.added ]; then
-            echo "Patch queue updated from ${GIT_TREE} ${GIT_BRANCH}" > $package.changes.proposed
-        fi
-        if [ -e qemu.changes.deleted ]; then
-            echo "* Patches dropped:" >> $package.changes.proposed
-            cat qemu.changes.deleted  >> $package.changes.proposed
-        fi
-        if [ -e qemu.changes.added ]; then
-            echo "* Patches added:" >> $package.changes.proposed
-            cat qemu.changes.added  >> $package.changes.proposed
-        fi
-        if [ -e $package.changes.proposed ]; then
-            osc vc --file=$package.changes.proposed $package
-            rm -f $package.changes.proposed
-        fi
-    done
-    if [ -e qemu.changes.deleted ]; then
-        rm -f qemu.changes.deleted
+patches=()
+for i in $CMP_DIR/*.tmp; do
+    basename="${i##*/}"
+    newname=${basename%.tmp}
+    newname=${newname%.diff} # remove .diff suffix it exist
+    # limit file names to 40 chars before extension
+    newname=${newname:0:40}.diff
+    # remove git signature and commit hash to make content
+    # independent of git version
+    head -n -3 "$i" | tail -n +2 > "$CMP_DIR/$newname"
+    rm "$i"
+    localname=${newname#*-}
+    patches+=("$localname")
+    if [ -e "$localname" ]; then
+	if cmp -s "$CMP_DIR/$newname" "$localname"; then
+	    rm "$CMP_DIR/$newname"
+	    let UNCHANGED_COUNT+=1
+	else
+	    mv "$CMP_DIR/$newname" "$localname"
+	    let CHANGED_COUNT+=1
+	fi
+    else
+	mv "$CMP_DIR/$newname" "$localname"
+	let ADDED_COUNT+=1
+	echo "  $localname" >> .update_git.changes.added
+	osc add "$localname"
     fi
-    if [ -e qemu.changes.added ]; then
-        rm -f qemu.changes.added
+done
+
+# delete dropped patches
+for patch in *.diff; do
+    keep=
+    for i in "${patches[@]}"; do
+	if [ "$i" = "$patch" ]; then
+	    keep=1
+	    break
+	fi
+    done
+    if [ -z "$keep" ]; then
+	osc rm --force $patch
+	let DELETED_COUNT+=1
+	echo "  $patch" >> .update_git.changes.deleted
     fi
-    echo "git patch summary"
-    echo "  unchanged: $UNCHANGED_COUNT"
-    echo "    changed: $CHANGED_COUNT"
-    echo "    deleted: $DELETED_COUNT"
-    echo "      added: $ADDED_COUNT"
-)
+done
 
-rm -rf $CMP_DIR
+for package in rpmlint; do
+    skip=
+    while IFS= read -r line; do
+	if [ "$line" = "# PATCHLIST END" ]; then
+	    skip=
+	    i=0
+	    for patch in "${patches[@]}"; do
+		printf "Patch%02d:        %s\n" "$i" "$patch"
+		let i+=1
+	    done
+	fi
+	if [ -z "$skip" ]; then
+	    echo "$line"
+	fi
+	if [ "$line" = "# PATCHLIST BEGIN" ]; then
+	    skip=1
+	fi
+    done < $package.spec > $package.spec.new
+    mv $package.spec.new $package.spec
 
-sed -e 's|^\(Name:.*qemu\)|\1-testsuite|' < qemu.spec > qemu-testsuite.spec
-osc service localrun format_spec_file
-
-/bin/sh pre_checkin.sh -q
-
-echo "Please remember to run pre_checkin.sh after modifying qemu.changes."
+    if [ -e .update_git.changes.deleted ]; then
+	echo "* Patches dropped:" >> $package.changes.proposed
+	cat .update_git.changes.deleted  >> $package.changes.proposed
+    fi
+    if [ -e .update_git.changes.added ]; then
+	echo "* Patches added:" >> $package.changes.proposed
+	cat .update_git.changes.added  >> $package.changes.proposed
+    fi
+    if [ -e $package.changes.proposed ]; then
+	osc vc --file=$package.changes.proposed $package
+	rm -f $package.changes.proposed
+    fi
+done
+rm -f .update_git.*
+echo "git patch summary"
+echo "  unchanged: $UNCHANGED_COUNT"
+echo "    changed: $CHANGED_COUNT"
+echo "    deleted: $DELETED_COUNT"
+echo "      added: $ADDED_COUNT"
